4c5d29bf5321dd9fac24e624fd66d4e12c218eb0
Akash Gupta <akashagupta@rediffmail.com>
2018-06-20 00:12:16 +0530

    ok now
    it is ok

--- moment.js
+++ moment.js
@@ -2,3 +2,8 @@ var moment = require('moment');
 var variable=moment('2010-10-22').isBefore('2010-10-21')
 console.log(variable)
 console.log('Hello')
+
+var moment = require('moment');
+var variable=moment('2010-10-22').isBefore('2010-10-21')
+console.log(variable)
+console.log('Hello')
